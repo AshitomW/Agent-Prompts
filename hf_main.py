573f9b01b04912b1370e78e8d642dd700df4e201
Ashitom <sasukeuchia32828@gmail.com>
2025-11-25 07:00:05 +0545

    hugging_face example

---
+++ hf_main.py
@@ -0,0 +1,10 @@
+from transformers import pipeline
+
+pipe = pipeline("text-generation",model="google/gemma-3-1b-it")
+messages = [
+    {"role": "user", "content": "Who are you?"},
+]
+pipe(messages)
+
+
+## Hugging Face Example
